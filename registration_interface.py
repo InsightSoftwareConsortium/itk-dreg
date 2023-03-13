cfe784022fb70fd6f29bd66a7003fba80b2363a8
HastingsGreer <hastings.greer@gmail.com>
2023-03-13 15:18:45 -0400

    Update registration_interface.py

--- registration_interface.py
+++ registration_interface.py
@@ -3,12 +3,16 @@ from abc import ABC, abstractmethod
 
 # The function signature each registration approach should provide
 
+FloatImage2DType = itk.Image[itk.F, 2]
+FloatImage3DType = itk.Image[itk.F, 3]
+FloatImageType = Union[Image2DType, Image3DType]
+
 class RegistrationMethod(ABC):
     @abstractmethod
     def __call__(
           self,
-          fixed_image: itk.Image[itk.F, 3],
-          moving_image: itk.Image[itk.F, 3],
+          fixed_image: FloatImageType,
+          moving_image: FloatImageType,
           initial_transform: itk.Transform[itk.D, 3],
           **kwargs
         ) -> itk.Transform[itk.D, 3]:
